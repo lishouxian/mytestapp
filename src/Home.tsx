386748d631cd68c763bc2e20922647999d69d927
lishouxian <li.shouxian@outlook.com>
2022-01-22 19:56:58 +0800

    新的 轮毂识别应用完成

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Upload, Icon, message, Alert } from 'antd';
-import {base64ToBlob, downloadFile} from "./utils";
+import {Alert, Upload} from 'antd';
+import {FileImageTwoTone} from '@ant-design/icons';
 import 'antd/dist/antd.css'
-interface IHomeProps {}
+
+interface IHomeProps {
+}
+
+let imgUrl = 'https://gitee.com/lishouxian/blog-img/raw/master/2022-01/061.jpg';
 
 
 const StyledContainer = styled.div`
     .upload-wrap {
         padding-top: 20px;
+    },
+    .img-wrap {
+        padding-top: 20px;
+        margin: auto;
+        width: 100%;
+
+    }
+    .img-wrap img {
+        display: block;
+        max-height: 500px;
+        max-width: 100%;
+        margin: auto;
     }
 `;
 
@@ -17,27 +33,21 @@ export const Home: React.FC<IHomeProps> = () => {
         name: 'file',
         multiple: false,
         withCredentials: false,
-        action: 'http://47.103.199.145:8008/translate',
-        onChange (info) {
-            const { status } = info.file;
+        action: 'http://10.11.111.42:8000/hello',
+        onChange(info) {
+            console.log(info.file.response);
+            const {status} = info.file;
             if (status !== 'uploading') {
                 console.log(info.file, info.fileList);
-                console.log(info.file.response);
-                const file = info.file;
-                if (file.response) {
-                    if (file.response.errorCode === 0) {
-                        const fileName = file.name.replace('.xlsx', '-完成处理.xlsx');
-                        const blob = base64ToBlob(file.response.payload, fileName);
-                        downloadFile(blob, fileName);
-                    } else {
-                        return message.error(`${info.file.name} 处理失败. ${file.response.errorMessage || ''}`);
-                    }
-                }
             }
-            if (status === 'done') {
-                message.success('处理成功，请检查下载的表格文件');
-            } else if (status === 'error') {
-                message.error(`${info.file.name} 上传失败，请检查xlsx文件.`);
+            console.log(info.file.response);
+            const file = info.file;
+            if (file.response) {
+                // @ts-ignore
+                document.getElementById('img-wrap').innerHTML = `<img src=${file.response.payload} className='img-class' alt={'sadsa'}/>`;
+                // @ts-ignore
+                // document.getElementById('img-wrap').getElementsByClassName('img-class').src = file.response.payload;
+                imgUrl = file.response.payload;
             }
             return true;
         }
@@ -47,7 +57,7 @@ export const Home: React.FC<IHomeProps> = () => {
         <Alert
             message={
                 <div>
-                    医学384pcr板数据处理工具，有问题请联系 CC98 @无羡
+                    机械工程学院机械设计研究所轮毂识别工具 @李寿贤
                 </div>
             }
             type="info"
@@ -56,11 +66,16 @@ export const Home: React.FC<IHomeProps> = () => {
         <div className="upload-wrap">
             <Upload.Dragger {...props}>
                 <p className="ant-upload-drag-icon">
-                    <Icon type="inbox" />
+                    <FileImageTwoTone />
                 </p>
-                <p className="ant-upload-text">将要处理的表格，拖到本区域</p>
-                <p className="ant-upload-hint">处理完成会自动下载，请耐心等待。</p>
+                <p className="ant-upload-text">请输入要处理的轮毂图片</p>
+                {/*<p className="ant-upload-hint">处理完成会自动下载，请耐心等待。</p>*/}
             </Upload.Dragger>
+        </div>
+
+        {/*<Image id="img" className='img-class' src = ""/>*/}
+        <div className="img-wrap" id="img-wrap">
+            <img src={imgUrl} className='img-class' alt={'sadsa'}/>
         </div>
     </StyledContainer>;
 };
